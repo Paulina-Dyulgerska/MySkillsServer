0af77cb27530fc29140f9570aec3a68189fa8e23
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-06-06 13:45:23 +0300

    Admin user Seeder add

--- MySkillsServer.Common/GlobalConstants.cs
+++ MySkillsServer.Common/GlobalConstants.cs
@@ -5,7 +5,7 @@
         public const string SystemName = "MySkillsServer";
 
         public const string AdministratorRoleName = "Administrator";
-        public const string GuestRoleName = "Guest";
+        public const string UserRoleName = "User";
 
         public const string JsonContentType = "application/json";
 
