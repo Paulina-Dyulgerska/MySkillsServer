81a6ad314ccb39022a5ab1e541deccf89079cdab
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-07-04 12:14:34 +0300

    Error and User response models add

---
+++ MySkillsServer.Common/ErrorResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MySkillsServer.Web.Common
+{
+    using System.Text.Json.Serialization;
+
+    public class ErrorResponseModel
+    {
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ Web/MySkillsServer.Web.Infrastructure/Middlewares/Authorization/TokenProviderUserResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MySkillsServer.Web.Infrastructure.Middlewares.Authorization
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    // ReSharper disable once InconsistentNaming
+    // [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+    public class TokenProviderUserResponseModel
+    {
+        // ReSharper disable once InconsistentNaming
+        // [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("userEmail")]
+        public string UserEmail { get; set; }
+
+        [JsonPropertyName("expiresIn")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("roles")]
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
